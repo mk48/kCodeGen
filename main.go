45f19cf3b27e512412a6d7de989e2a8b2bd54c17
Muthu Kumaran <mkumaran@xolta.com>
2024-06-27 21:44:54 +0200

    service get

--- main.go
+++ main.go
@@ -11,39 +11,51 @@ import (
 )
 
 type CodeGen struct {
-	TableName string
-	Columns   []Column
+	TableName              string
+	AliasTableNameInSelect string
+	Columns                []Column
+}
+
+type RefTable struct {
+	Name          string
+	SelectModel   string
+	SelectColumns []string
 }
 
 type Column struct {
 	Name        string
 	DataType    string //uuid, string, time,
 	DataTypeLen int    // varchar(*)
-	RefTable    string
+	RefTable    *RefTable
 	IsNull      bool
-
-	IsIndexed bool
+	IsIndexed   bool
 }
 
 func main() {
 	columns := []Column{
 		{
-			Name:      "user_id",
-			DataType:  "uuid",
-			RefTable:  "users",
-			IsNull:    false,
-			IsIndexed: true,
+			Name:        "name",
+			DataType:    "string",
+			DataTypeLen: 100,
+			IsNull:      false,
+			IsIndexed:   true,
 		},
 		{
-			Name:        "role",
+			Name:        "mobile_country_code",
+			DataType:    "string",
+			DataTypeLen: 10,
+			IsNull:      true,
+		},
+		{
+			Name:        "mobile_no",
 			DataType:    "string",
 			DataTypeLen: 20,
-			IsNull:      false,
+			IsNull:      true,
 		},
 		{
 			Name:     "created_by",
 			DataType: "uuid",
-			RefTable: "users",
+			RefTable: &RefTable{Name: "users", SelectModel: "IdEmailModel", SelectColumns: []string{"id", "email"}},
 			IsNull:   false,
 		},
 		{
@@ -52,18 +64,20 @@ func main() {
 			IsNull:   false,
 		},
 		{
-			Name:     "deleted_by",
+			Name:     "updated_by",
 			DataType: "uuid",
-			RefTable: "users",
+			RefTable: &RefTable{Name: "users", SelectModel: "IdEmailModel", SelectColumns: []string{"id", "email"}},
 			IsNull:   true,
 		},
 		{
-			Name:      "deleted_at",
+			Name:      "updated_at",
 			DataType:  "time",
 			IsNull:    true,
 			IsIndexed: true,
 		},
 	}
+
+	codeGen := CodeGen{TableName: "persons", AliasTableNameInSelect: "p", Columns: columns}
 
 	//----------- func
 	createColumn := func(column Column) string {
@@ -86,11 +100,19 @@ func main() {
 		}
 
 		var ref = ""
-		if column.RefTable != "" {
-			ref = fmt.Sprintf("REFERENCES %s(id)", column.RefTable)
+		if column.RefTable != nil {
+			ref = fmt.Sprintf("REFERENCES %s(id)", column.RefTable.Name)
 		}
 
-		return fmt.Sprintf("%s %s %s %s", column.Name, dataType, isNull, ref)
+		return fmt.Sprintf("\"%s\" %s %s %s", column.Name, dataType, isNull, ref)
+	}
+
+	left := func(str string) string {
+		return str[0:1]
+	}
+
+	notNull := func(n interface{}) bool {
+		return n != nil
 	}
 
 	sub := func(n1 int, n2 int) int {
@@ -100,9 +122,40 @@ func main() {
 	camelCase := func(str string) string {
 		return xstrings.ToCamelCase(str)
 	}
-	//----------- func - end
 
-	codeGen := CodeGen{TableName: "users_role", Columns: columns}
+	pascalCase := func(str string) string {
+		return xstrings.ToPascalCase(str)
+	}
+
+	createColumnForStruct := func(column Column) string {
+		var goDataType = ""
+		switch column.DataType {
+		case "uuid":
+			goDataType = "string"
+		case "time":
+			goDataType = "time.Time"
+		case "string":
+			goDataType = "string"
+		case "number":
+			goDataType = "int"
+			if column.DataTypeLen >= 0 {
+				goDataType = fmt.Sprintf("int%d", column.DataTypeLen)
+			}
+		}
+
+		if column.RefTable != nil {
+			goDataType = column.RefTable.SelectModel
+		}
+
+		nullIndicator := ""
+		if column.IsNull {
+			nullIndicator = "*"
+		}
+		goDataTypeWithNull := fmt.Sprintf("%s%s", nullIndicator, goDataType)
+
+		return fmt.Sprintf("%s\t%s\t`json:\"%s\" db:\"%s\"`", xstrings.ToPascalCase(column.Name), goDataTypeWithNull, xstrings.ToCamelCase(column.Name), column.Name)
+	}
+	//----------- func - end
 
 	//read template folder
 	templateFiles, err := os.ReadDir("./tmpl/")
@@ -115,9 +168,13 @@ func main() {
 
 		baseName := path.Base(templateFileName)
 		tmpl, err := template.New(baseName).Funcs(template.FuncMap{
-			"createColumn": createColumn,
-			"sub":          sub,
-			"camelCase":    camelCase,
+			"left":                  left,
+			"notNull":               notNull,
+			"createColumn":          createColumn,
+			"sub":                   sub,
+			"camelCase":             camelCase,
+			"pascalCase":            pascalCase,
+			"createColumnForStruct": createColumnForStruct,
 		}).ParseFiles(templateFileName)
 		if err != nil {
 			panic(err)
